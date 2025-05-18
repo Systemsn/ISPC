9b8764e3c1feada64df3d9505d3d0c3aa5648047
Luis David Flores PC <Floresluisutn@gmail.com>
2025-05-18 17:16:05 -0300

    Se modifico panel principal, se agrego sub paneles para cada funcion. Se Agregaron Fuciones Prender_Luces,Apagar_Luces,Mostrar_luces_Prendidos,Mostrar_Luces_Apagado,mostrar_luces que es global,luces_modo_noche,luces_modo_ahorro

--- proyecto_Evidencia2/main.py
+++ proyecto_Evidencia2/main.py
@@ -1,54 +1,218 @@
 
-import modulo
+import modulo # importamos todos los metodos del modulo.py
 
-
-
-while True:
+if __name__ == "__main__":
     
-    print(":::::Menu:::::")
-    print("Opcion 1 : Agregar Dispositivo")
-    print("Opcion 2 : Busar Dispositivo")
-    print("Opcion 3 : Mostrar Dispositivo")
-    print("Opcion 4 : Eliminar Dispostivos")
-    opcion  = int(input("Ingrese la opcion: "))
-    match opcion:
-    
-        case 1 : 
-             # nombre,marca,modelo,tipo,color,consumo_energia,voltaje
-             
-             nombre_dispositivo = input("Ingrese el nombre del Dispositivo: ") # 
-             marca = input("Ingrese la marca del dispositivo: ")
-             modelo = input("Ingrese el modelo: ")
-             tipo = input("Ingrese el tipo: ")
-             color = input("Ingrese el color : ")
-             consumo_energia = int(input("ingrese el consumo de energia del dispositivo: "))
-             voltaje = int(input("Ingrese el voltaje del Electrodomostico: "))
-             modulo.agregar(nombre_dispositivo,marca,modelo,tipo,color,consumo_energia,voltaje)
-             
-             pass
-        case 2 : 
-              busqueda = input("Ingrese el nombre del dispositivo que desea encontrar: ")
+ 
 
+    while True: # Creamos un funcion repetitiva para que se pueda usar nuevamente el menu 
+    # Creamos el menu donde damos opciones para el usuario. 
+        print("\n::::::::::::::::::::::::::::::")
+        print("::      PANEL PRINCIPAL     ::")
+        print("::::::::::::::::::::::::::::::")
+        
+        
+        print("Opcion 1 : Panel Dispositivos")
+        print("Opcion 2 : Panel de Iluminacion")
+        print("Opcion 6 : Salir del programa")
+        
+        opcion  = int(input("Ingrese la opcion: ")) # Guardamos la opcion del usuario 
+        match opcion:# ejecuta la opcion del usuario
+    
+                case 1 : 
+                    while True : 
+                        print("\n::::::::::::::::::::::::::::::")
+                        print("::    PANEL DISPOSITIVOS    ::")
+                        print("::::::::::::::::::::::::::::::")
+                        print("\nOpcion 1 : Agregar nuevo dispositivo")
+                        print("Opcion 2 : Buscar dispositivo")
+                        print("Opcion 3 : Mostrar todo los dispositivos")
+                        print("Opcion 4 : Eliminar Dispositivos")
+                        print("Opcion 5 : Volver al menu anterior")
+                        opcion = int(input("Ingrese la opcion: "))
+                        match opcion:
+                            
+                            case 1: 
+                         
+                                # desde la linea 21 al 33 creamos las variables y pedimos al usuario por teclado.
+        
+                                nombre_dispositivo = input("\nIngrese el nombre del Dispositivo: ").lower()  
+                    
+                                marca = input("Ingrese la marca del dispositivo: ").lower()
+                    
+                                modelo = input("Ingrese el modelo: ").lower()
+                                
+                                print("\n::::::::::::::::::::::::::::::")
+                                print("::  TIPOS DE DISPOSITIVOS   ::")
+                                print("::::::::::::::::::::::::::::::")
+                                print("1. Electrodomesticos")
+                                print("2. Iluminacion")
+                                print("3. Seguridad")
+                               
+                                tipo = int(input("Ingrese el tipo: "))
+                    
+                                color = input("Ingrese el color : ").lower()
+                                temperatura = int(input("Ingrese la temperatura en º C | Si no tiene ingrese 0 : "))
+                                
+                                tiempo = 0
+                    
+                                consumo_energia = int(input("ingrese el consumo de energia del dispositivo: "))
+                    
+                                voltaje = int(input("Ingrese el voltaje del Electrodomostico: "))
+                                
+                                ubicacion = input("Ingrese la ubicacion donde se colocara: ").lower()
+                                estado = 0 
+                    
+                                modulo.agregar(nombre_dispositivo,marca,modelo,tipo,color,temperatura,tiempo,consumo_energia,voltaje,ubicacion,estado) # Aqui agregamos los datos a la funcion agregar.
+
+
+                            case 2 : 
+
+                                busqueda = input("\nIngrese el nombre del dispositivo que desea encontrar: ") # ingresamos el dispositivo a buscar
+                                print("\n::::::::::::::::::::::::::::::")
+                                print("::  Dispositivo Encontrado  ::")
+                                print("::::::::::::::::::::::::::::::")
               
-              print(modulo.buscar(busqueda))
-              pass
+                                print(modulo.buscar(busqueda))
+                                pass
         
-        case 3 : 
-            print(modulo.mostrar())
-            pass
+                            case 3 : 
+                                print("\n::::::::::::::::::::::::::::::")
+                                print("::  Todos los dispositivos  ::")
+                                print("::::::::::::::::::::::::::::::")
+                                modulo.mostrar()# aqui imprimimos la funcion donde se muestra la listas y sus datos
+                 
     
-        case 4 :
-            busqueda = input("Ingrese el dispositivo que desea eliminar: ")
+                            case 4 :
+                                
+                                
+                                busqueda = input("\nIngrese el dispositivo que desea eliminar: ") # ingresamos el dispositivo a eliminar 
+                                print("\n::::::::::::::::::::::::::::::")
+                                print("::  Dispositivo Eliminado   ::")
+                                print("::::::::::::::::::::::::::::::")
              
-            modulo.eliminar(busqueda)
-            pass 
-            
-        case _:
-            print("Error : ingreso una opcion no valida ")
-            continue
+                                print(modulo.eliminar(busqueda)) # Aqui imprimimos y llamamos a la funcion para que elimine el dispositivo
+                                pass 
+                                
+                            case 5: 
+                                break
+                            case _:
+                                # Este es un caso por default donde si no ingresan correctamente las opciones que indica el menu.
+                                print("Error : ingreso una opcion no valida ") # imprimimos el error 
+                                continue
         
-    pregunta = input("Quiere ingresar otra opcion?: ").lower().strip()
-    if pregunta == "no":
-        break
+                case 2 :  
+                    while True:
+                        
+                        print("\n::::::::::::::::::::::::::::::")
+                        print("::   PANEL DE ILUMINACION   ::")
+                        print("::::::::::::::::::::::::::::::")
+
+                        print("Opcion 1 : Prender / Apagar luces")
+                        print("Opcion 2 : Mostrar Prendio/ Apagado / todo")
+                        print("Opcion 3 : Configurar Modo noche")
+                        print("Opcion 4 : Configurar Modo Ahorro de Energia")
+                        print("Opcion 5 : Volver al menu principal")
+                        opcion = int(input("Ingrese una opcion: "))
+                        match opcion :
+                            case 1: 
+                                print("\n:::::::::::::::::::::::::::::::::")
+                                print(":: PANEL DE PRENDIDO Y APAGADO ::")
+                                print(":::::::::::::::::::::::::::::::::")
+                                print("1.Prender luces")
+                                print("2.Apagar luces")
+                                opcion = int(input("Ingrese una opcion: "))
+                                
+                                
+                                if opcion ==1 :
+                                    busqueda = input(" Que habitacion quiere prender las luces?: ")
+                                    
+                                    modulo.prender_luces(busqueda)
+                                elif opcion ==2:
+                                    busqueda = input(" Que habitacion quiere pagar las luces?: ")
+                                    modulo.apagar_luces()
+                            case 2:
+                                
+                                print("\n:::::::::::::::::::::::::::::::::")
+                                print(":: PANEL MOSTRAR LUCES ::")
+                                print(":::::::::::::::::::::::::::::::::")
+                                print("1.Mostrar habitaciones apagadas")
+                                print("2.Mostrar habitaciones prendidas")
+                                print("3.Mostrar todas las habitaciones")
+                                print("4.Volver al menu anterior")
+                                opcion = int(input("Ingrese una opcion: "))
+                                
+                                match opcion:
+                                    case 1 : 
+                                        print("\n:::::::::::::::::::")
+                                        print(":: LUCES APAGADA ::")
+                                        print(":::::::::::::::::::")
+                                        modulo.mostrar_luces_apagado()
+                                        
+                                        pass
+                                    
+                                    case 2:
+                                        print("\n:::::::::::::::::::")
+                                        print(":: LUCES PRENDIDAS ::")
+                                        print(":::::::::::::::::::")
+                                        modulo.mostrar_luces_prendidos()
+                                        
+                                        pass
+                                    
+                                    case 3: 
+                                        print("\n::::::::::::::::::::::::")
+                                        print(":: TODAS HABITACIONES ::")
+                                        print("::::::::::::::::::::::::")
+                                        modulo.mostrar_luces()
+                                        
+                                        pass
+                                    
+                                    case 4: 
+                                    
+                                        break
+                                    case _:
+                                        print("Error : Ingreso una opcion no valida")
+                                    
+                            case 3: 
+                                print("\n:::::::::::::::::::::::::::::")
+                                print(":: CONFIGURACION MODO NOCHE ::")
+                                print("::::::::::::::::::::::::::::::")
+                                        
+                                hora_prendido= int(input("Ingrese la hora a que se prenderan las luces "))
+                                while True:
+                                            
+                                        
+                                    ubicacion_exterior = input("Ingrese la ubicacion de la casa: ")
+    
+                                    modulo.luces_modo_Noche(hora_prendido,ubicacion_exterior)
+                                            
+                                    pregunta = input("Quiere Ingresar otra ubicacion de la casa? SI O NO").lower().strip()
+                                    if pregunta == "no":
+                                        break
+                                       
+                                    
+                            case 4:
+                                print("\n::::::::::::::::::::::::::::::")
+                                print(":: CONFIGURACION MODO AHORRO ::")
+                                print(":::::::::::::::::::::::::::::::")
+                                        
+                                hora_apagado= int(input("Ingrese la hora a que se apagaran las luces "))
+                                while True:
+                                            
+                                        
+                                    ubicacion_exterior = input("Ingrese la ubicacion de la casa: ")
+    
+                                    modulo.luces_modo_Noche(hora_apagado,ubicacion_exterior)
+                                            
+                                    pregunta = input("Quiere Ingresar otra ubicacion de la casa? SI O NO").lower().strip()
+                                    if pregunta == "no":
+                                        break
+                                    
+                    
+        
+        
+        #pregunta = input("Quiere ingresar otra opcion?: ").lower().strip()
+       # if pregunta == "no":
+        # break
         
     
--- proyecto_Evidencia2/modulo.py
+++ proyecto_Evidencia2/modulo.py
@@ -1,29 +1,36 @@
-dispositivos =[]
+from datetime import datetime
+list_dispositivo =[]
 
  
-def agregar(nombre_dispositivo,marca,modelo,tipo,color,consumo_energia,voltaje):
+def agregar(nombre_dispositivo,marca,modelo,tipo,color,temperatura,tiempo,consumo_energia,voltaje,ubicacion,estado):
     
     
-    dispositivo = {"nombre_dispositivo":nombre_dispositivo,"marca":marca,"modelo":modelo,"tipo":tipo,"color":color, 
-                   "consumo_energia": consumo_energia,"voltaje":voltaje}
+    dispositivo = {"nombre_dispositivo":nombre_dispositivo,"marca":marca,"modelo":modelo,"tipo":tipo,"color":color,"temperatura":temperatura,"tiempo":tiempo, 
+                   "consumo_energia": consumo_energia,"voltaje":voltaje,"ubicacion":ubicacion,"estado":estado}
     
-    dispositivos.append(dispositivo)    
+    list_dispositivo.append(dispositivo)  
+    print("\n Se agrego con exito")
+    
+     
     
 
 def mostrar(): # Funcion para mostar todos los dispositivos
     
-    if len(dispositivos)== 0:
+    if len(list_dispositivo)== 0:
         return "Sin dispositivos"
     
-    for dispositivo in dispositivos:
+    
+    for dispositivo in list_dispositivo:
+        encontrado = True
         
         print(dispositivo)
+            
 
 def buscar(busqueda):
     
     encontrado = False
     
-    for dispositivo in dispositivos:
+    for dispositivo in list_dispositivo:
         
         if dispositivo['nombre_dispositivo'] == busqueda:
             encontrado = True
@@ -36,11 +43,115 @@ def eliminar(busqueda):
     
     encontrado = False
     
-    for dispositivo in dispositivos:
+    for dispositivo in list_dispositivo:
         
         if dispositivo['nombre_dispositivo'] == busqueda:
             encontrado = True
-            dispositivos.remove(dispositivo)
-         print(f"{dispositivo} Se elimino con exito")
+            list_dispositivo.remove(dispositivo)
+            return(dispositivo)
+        
+        
+    if encontrado == True:
+        print("Se Elimino con exito")
+        
     if encontrado== False:
         print("No se encontro el dispositivo")
+
+def apagar_luces(busqueda):
+    
+    for dispositivo in list_dispositivo:
+        
+        if dispositivo["tipo"] == 2 and dispositivo["ubicacion"] == busqueda:
+            
+            dispositivo["estado"]=0
+            
+def prender_luces(busqueda):
+    
+    for dispositivo in list_dispositivo:
+        
+        if dispositivo["tipo"] == 2 and dispositivo["ubicacion"] == busqueda:
+            
+            dispositivo["estado"]=1
+
+def mostrar_luces_prendidos():
+    for dispositivo in list_dispositivo:
+        
+        if dispositivo["estado"] == 1 and dispositivo["tipo"]==2:
+            
+            print(f'Las luces de {dispositivo["ubicacion"]} esta  prendido')
+
+def mostrar_luces_apagado():
+    for dispositivo in list_dispositivo:
+        
+        if dispositivo["estado"] == 0 and dispositivo["tipo"]==2:
+            
+            print(f' Las luces de {dispositivo["ubicacion"]} esta  apagado')
+            
+def mostrar_luces():
+    
+    for dispositivo in list_dispositivo:
+        
+        if dispositivo["tipo"] == 2:
+            
+            if dispositivo["estado"] == 0:
+                print(f'Las luces de {dispositivo["ubicacion"]} esta  apagado')
+            elif dispositivo["estado"] ==1 :
+                print(f'Las luces de {dispositivo["ubicacion"]} esta  prendididas ')
+
+def luces_modo_Noche(hora_prendido,minuto_prendido,ubicacion_exterior):
+    # obtenemos la hora actual del sistema
+    ahora = datetime.now()
+    
+    hora_actual = ahora.hour 
+    minuto_actual = ahora.minute
+    
+    if hora_prendido == hora_actual and minuto_actual == minuto_prendido:
+        
+        encendido = False # variable para verificar si se encontro alguna luz prendida
+        
+        for dispositivo in list_dispositivo:
+            
+            if dispositivo["tipo"] == 2 and dispositivo["ubicacion"] ==  ubicacion_exterior:
+                 
+                print(f'Se encendió la luz de {dispositivo["ubicacion"]} con éxito.')
+                
+                dispositivo["estado"] = 1  # encendemo la luz 
+                
+                encendido = True # indicamos que se encontro y se prendio una luz
+             
+            # Si no se encontró ningún dispositivo compatible
+        if not encendido:
+            print(f" No se encontró ningún dispositivo de iluminación en '{ubicacion_exterior}'.")
+    else:
+        print(f" No es la hora configurada aún. Hora actual: {hora_actual}, hora configurada: {hora_prendido}")
+                 
+   
+            
+def luces_modo_ahorro(hora_apagado,minuto_apagado,ubicacion_exterior):
+    # obtenemos la hora actual del sistema
+    ahora = datetime.now()
+    
+    hora_actual = ahora.hour 
+    
+    minuto_actual = ahora.minute
+    
+    if hora_apagado == hora_actual and minuto_actual == minuto_apagado:
+        
+        encendido = False # variable para verificar si se encontro alguna luz prendida
+        
+        for dispositivo in list_dispositivo:
+            
+            if dispositivo["tipo"] == 2 and dispositivo["ubicacion"] ==  ubicacion_exterior:
+                 
+                print(f'Se encendió la luz de {ubicacion_exterior} con éxito.')
+                
+                dispositivo["estado"] = 0  # encendemo la luz 
+                
+                encendido = True # indicamos que se encontro y se prendio una luz
+             
+            # Si no se encontró ningún dispositivo compatible
+        if not encendido:
+            print(f" No se encontró ningún dispositivo de iluminación en '{ubicacion_exterior}'.")
+    else:
+        print(f" No es la hora configurada aún. Hora actual: {hora_actual}, hora configurada: {hora_apagado}")
+                 
